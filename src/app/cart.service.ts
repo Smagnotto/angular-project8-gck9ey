41666f002c689c8bb7a7f5a129d7e46aef57d627
Smagnotto <diego.smagnotto@gmail.com>
2019-08-30 20:36:35 -0300

    Initial commit

---
+++ src/app/cart.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable()
+export class CartService {
+  items = [];
+
+  constructor(
+    private http: HttpClient
+  ) {}
+
+
+  addToCart(product) {
+    this.items.push(product);
+  }
+
+  removeItem(itemId) {
+    return this.items.splice(itemId, 1);
+  }
+
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+}
